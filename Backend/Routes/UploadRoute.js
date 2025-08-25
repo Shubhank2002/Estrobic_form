8ef2997cc91618fd921b8b9eda9bfd55f784efcc
SHUBHANK MAHESHWARI <maheshwarishubhank2002@gmail.com>
2025-08-26 02:45:50 +0530

    new changes

--- Backend/Routes/UploadRoute.js
+++ Backend/Routes/UploadRoute.js
@@ -5,6 +5,6 @@ const { UploadFile } = require('../Controllers/UploadController')
 const upload = require('../Middlewares/Multer')
 const uploadRouter=express.Router()
 
-uploadRouter.post('/upload',upload.single('resume'),UploadFile)
+uploadRouter.post('/upload',upload.single('file'),UploadFile)
 
 module.exports=uploadRouter
